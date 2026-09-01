b1fc7ffc209a507e28537e04484ab784880ca132
Mufazzalshokh Ulashev <78840157+mufazzalshokh@users.noreply.github.com>
2026-09-01 18:39:04 +0500

    Update quotes in rails_helper.rb to double quotes

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,9 +1,9 @@
 # spec/rails_helper.rb
-ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../config/environment', __dir__)
+ENV["RAILS_ENV"] ||= "test"
+require File.expand_path("../config/environment", __dir__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
-require 'rspec/rails'
-require 'webmock/rspec'
+require "rspec/rails"
+require "webmock/rspec"
 
 # Configure WebMock to prevent real HTTP requests
 WebMock.disable_net_connect!(allow_localhost: true)
